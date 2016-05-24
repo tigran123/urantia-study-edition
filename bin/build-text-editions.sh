d93de82352565eecb378a870701cb5c8149e01ae
Tigran Aivazian <aivazian.tigran@gmail.com>
2016-05-24 12:54:40 +0100

    Don't try to build PDFs using Newton typeface family.

--- bin/build-text-editions.sh
+++ bin/build-text-editions.sh
@@ -24,7 +24,7 @@ function unset_tag()
 
 function set_pgkoboaurahd()
 {
-	echo -n "Building Kobo Aura HD PDFs: "
+	echo "Building Kobo Aura HD PDFs"
 	unset_tag pgkobomini
 	unset_tag pgkindledx
 	unset_tag pgnexus7
@@ -46,7 +46,7 @@ set_tag nofnt
 set_tag noquiz
 set_pgkoboaurahd
 
-fontlist="garamond goudy minionpro academy palatino oldstandard gentium bookman arno century cambria agora newton fedra cent adamant swift charter maiola"
+fontlist="garamond goudy minionpro academy palatino oldstandard gentium bookman arno century cambria agora fedra cent adamant swift charter maiola"
 for font in $fontlist
 do
   unset_tag $font
@@ -57,5 +57,4 @@ do
    set_tag $font
    make vclean ; make && mv -f ${MOD}.pdf ${OUT}/Revelation-Text-7in-${font}.pdf
    unset_tag $font
-   echo -n "$font"
 done
