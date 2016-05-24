60c9651b1aac8043bcc5f8a115ee4737594d9fae
Tigran Aivazian <aivazian.tigran@gmail.com>
2016-05-24 12:14:43 +0100

    Print section title in bold and adjust some font sizes.

--- bin/build-text-editions.sh
+++ bin/build-text-editions.sh
@@ -24,11 +24,13 @@ function unset_tag()
 
 function set_pgkoboaurahd()
 {
-	echo "Building Kobo Aura HD PDF"
+	echo -n "Building Kobo Aura HD PDFs: "
 	unset_tag pgkobomini
 	unset_tag pgkindledx
 	unset_tag pgnexus7
 	unset_tag pghanlin
+	unset_tag pgcrownq
+	unset_tag pgafour
 	set_tag pgkoboaurahd
 }
 
@@ -37,12 +39,14 @@ rm -rf $OUT ; mkdir -p $OUT
 unset_tag private
 unset_tag introinclude
 unset_tag fancylettrine
-unset_tag nofancydecor
+unset_tag coverimage
+set_tag nofancydecor
 unset_tag pictures
+set_tag nofnt
 set_tag noquiz
 set_pgkoboaurahd
 
-fontlist="garamond goudy minionpro academy palatino oldstandard gentium bookman arno century cambria agora newton fedra cent adamant swift bliss"
+fontlist="garamond goudy minionpro academy palatino oldstandard gentium bookman arno century cambria agora newton fedra cent adamant swift charter maiola"
 for font in $fontlist
 do
   unset_tag $font
@@ -51,6 +55,7 @@ done
 for font in $fontlist
 do
    set_tag $font
-   make vclean ; make && mv -f ${MOD}.pdf ${OUT}/urantia-text-edition-${font}.pdf
+   make vclean ; make && mv -f ${MOD}.pdf ${OUT}/Revelation-Text-7in-${font}.pdf
    unset_tag $font
+   echo -n "$font"
 done
