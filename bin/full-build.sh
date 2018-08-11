bc88a28e93635b6d51cdb1d6bb0372bc3a7cd493
Tigran Aivazian <aivazian.tigran@gmail.com>
2018-08-11 17:22:50 +0100

    Configure the "big tablet" build more completely.

--- bin/full-build.sh
+++ bin/full-build.sh
@@ -4,7 +4,7 @@ MOD=urantia-study-edition
 STYFILE=${MOD}.sty
 OUT=pdf
 OUTFILE=Revelation
-FORMATS="8in 7in 10in A4 tablet 5in"
+FORMATS="tablet bigtablet 5in 7in 8in 10in A4"
 
 function set_tag()
 {
@@ -47,43 +47,50 @@ function unset_tags()
 function set_pg10in()
 {
    set_tags "afterpartnewpage papernewpage introinclude coverimage arno pgkindledx nofancydecor"
-   unset_tags "beforepartnewpage noquiz pgcrownq garamond cent pgkobomini pgkoboaurahd pgauraone pgafour pgnexus7"
+   unset_tags "beforepartnewpage noquiz pgcrownq garamond pgkobomini pgkoboaurahd pgauraone pgafour pgnexus7 pgnexus10"
 }
 
 function set_pg5in()
 {
     set_tags "afterpartnewpage papernewpage introinclude coverimage arno nofancydecor pgkobomini"
-    unset_tags "beforepartnewpage noquiz garamond cent pgcrownq pgkindledx pgkoboaurahd pgauraone pgafour pgnexus7"
+    unset_tags "beforepartnewpage noquiz garamond pgcrownq pgkindledx pgkoboaurahd pgauraone pgafour pgnexus7 pgnexus10"
 }
 
 function set_pg7in()
 {
     set_tags "afterpartnewpage papernewpage nofancydecor introinclude coverimage garamond pgkoboaurahd"
-    unset_tags "beforepartnewpage noquiz pgcrownq pgkobomini arno garamond cent pgkindledx pgafour pgnexus7 pgauraone"
+    unset_tags "beforepartnewpage noquiz pgcrownq pgkobomini arno pgkindledx pgafour pgnexus7 pgauraone pgnexus10"
 }
 
 function set_pg8in()
 {
     set_tags "afterpartnewpage papernewpage nofancydecor introinclude coverimage garamond pgauraone"
-    unset_tags "beforepartnewpage noquiz pgcrownq pgkobomini arno cent pgkindledx pgafour pgnexus7 pgkoboaurahd"
+    unset_tags "beforepartnewpage noquiz pgcrownq pgkobomini arno pgkindledx pgafour pgnexus7 pgkoboaurahd pgnexus10"
 }
 
 function set_pgtablet()
 {
-    set_tags "afterpartnewpage papernewpage inofancydecor ntroinclude coverimage cent pgnexus7"
-    unset_tags "beforepartnewpage noquiz pgcrownq garamond pgkobomini pgkindledx pgkoboaurahd pgauraone pgafour"
+    set_tags "afterpartnewpage papernewpage inofancydecor ntroinclude coverimage garamond pgnexus7"
+    unset_tags "beforepartnewpage noquiz pgcrownq garamond pgkobomini pgkindledx pgkoboaurahd pgauraone pgafour pgnexus10"
 }
+
+function set_pgbigtablet()
+{
+    set_tags "afterpartnewpage papernewpage inofancydecor ntroinclude coverimage garamond pgnexus10"
+    unset_tags "beforepartnewpage noquiz pgcrownq garamond pgkobomini pgkindledx pgkoboaurahd pgauraone pgafour pgnexus7"
+}
+
 
 function set_pgA4()
 {
     set_tags "afterpartnewpage papernewpage noquiz introinclude coverimage nofancydecor arno pgafour"
-    unset_tags "beforepartnewpage pgcrownq pgkobomini pgkindledx garamond cent pgkoboaurahd pgauraone pgnexus7"
+    unset_tags "beforepartnewpage pgcrownq pgkobomini pgkindledx garamond pgkoboaurahd pgauraone pgnexus7 pgnexus10"
 }
 
 function set_pgcrownq()
 {
     set_tags "beforepartnewpage papernewpage noquiz nofancydecor garamond pgcrownq"
-    unset_tags "afterpartnewpage introinclude coverimage arno cent pgafour pgkobomini pgkindledx pgkoboaurahd pgauraone pgnexus7"
+    unset_tags "afterpartnewpage introinclude coverimage arno pgafour pgkobomini pgkindledx pgkoboaurahd pgauraone pgnexus7 pgnexus10"
 }
 
 
@@ -94,9 +101,16 @@ function build_all()
 
     for fmt in $FORMATS
     do
-	   set_pg${fmt}
+       set_pg${fmt}
        echo -n "$fmt: "
-	   make vclean ; make -s && mv -f ${MOD}.pdf ${outdir}/${OUTFILE}-${fmt}.pdf
+       make vclean
+       make -s
+       if [ $? -eq 0 ] ; then
+         mv -f ${MOD}.pdf ${outdir}/${OUTFILE}-${fmt}.pdf
+       else
+          echo "ERROR: Build failed, please examine the logs"
+          exit
+       fi
        if [ "$flag" = "public" ] ; then
           cd ${outdir}
           zip -q ${OUTFILE}-${fmt}.pdf.zip ${OUTFILE}-${fmt}.pdf
