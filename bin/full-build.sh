5b53f55837e5c61f63c34582edbec6ae9dc102ea
Tigran Aivazian <aivazian.tigran@gmail.com>
2022-06-01 18:33:06 +0100

    Build 8in with Caslon and print build times

--- bin/full-build.sh
+++ bin/full-build.sh
@@ -47,50 +47,50 @@ function unset_tags()
 function set_pg5in()
 {
     set_tags "bible afterpartnewpage papernewpage garamond pgkobomini"
-    unset_tags "beforepartnewpage noquiz basker pghanlin pgcrownq pgkindledx pgkoboaurahd pgauraone pgafour pgnexus7 pgnexus10 pgthinmob"
+    unset_tags "beforepartnewpage noquiz basker pghanlin pgcrownq pgkindledx pgkoboaurahd pgauraone pgafour pgnexus7 pgnexus10 pgthinmob caslon"
 }
 
 function set_pg7in()
 {
     set_tags "bible afterpartnewpage papernewpage garamond pgkoboaurahd"
-    unset_tags "beforepartnewpage noquiz pghanlin pgcrownq pgkobomini pgkindledx pgafour basker pgnexus7 pgauraone pgnexus10 pgthinmob"
+    unset_tags "beforepartnewpage noquiz pghanlin pgcrownq pgkobomini pgkindledx pgafour basker pgnexus7 pgauraone pgnexus10 pgthinmob caslon"
 }
 
 function set_pg6in()
 {
     set_tags "bible afterpartnewpage papernewpage basker pghanlin"
-    unset_tags "beforepartnewpage noquiz pgcrownq pgkoboaurahd pgkobomini pgkindledx pgafour garamond pgnexus7 pgauraone pgnexus10 pgthinmob"
+    unset_tags "beforepartnewpage noquiz pgcrownq pgkoboaurahd pgkobomini pgkindledx pgafour garamond pgnexus7 pgauraone pgnexus10 pgthinmob caslon"
 }
 
 function set_pg8in()
 {
-    set_tags "bible afterpartnewpage papernewpage garamond pgauraone"
-    unset_tags "beforepartnewpage noquiz pghanlin pgcrownq pgkobomini pgkindledx basker pgafour pgnexus7 pgkoboaurahd pgnexus10 pgthinmob"
+    set_tags "bible afterpartnewpage papernewpage caslon pgauraone"
+    unset_tags "beforepartnewpage noquiz pghanlin pgcrownq pgkobomini pgkindledx basker pgafour pgnexus7 pgkoboaurahd pgnexus10 pgthinmob garamond"
 }
 
 function set_pgthinmob()
 {
     set_tags "bible afterpartnewpage papernewpage ntroinclude garamond pgthinmob"
-    unset_tags "beforepartnewpage noquiz pghanlin pgcrownq pgkobomini pgkindledx pgkoboaurahd basker pgauraone pgafour pgnexus10 pgnexus7"
+    unset_tags "beforepartnewpage noquiz pghanlin pgcrownq pgkobomini pgkindledx pgkoboaurahd basker pgauraone pgafour pgnexus10 pgnexus7 caslon"
 }
 
 
 function set_pgmobile()
 {
     set_tags "bible afterpartnewpage papernewpage ntroinclude garamond pgnexus7"
-    unset_tags "beforepartnewpage noquiz pghanlin pgcrownq pgkobomini pgkindledx pgkoboaurahd basker pgauraone pgafour pgnexus10 pgthinmob"
+    unset_tags "beforepartnewpage noquiz pghanlin pgcrownq pgkobomini pgkindledx pgkoboaurahd basker pgauraone pgafour pgnexus10 pgthinmob caslon"
 }
 
 function set_pgtablet()
 {
     set_tags "bible afterpartnewpage papernewpage ntroinclude garamond pgnexus10"
-    unset_tags "beforepartnewpage noquiz pghanlin pgcrownq pgkobomini pgkindledx pgkoboaurahd basker pgauraone pgafour pgnexus7 pgthinmob"
+    unset_tags "beforepartnewpage noquiz pghanlin pgcrownq pgkobomini pgkindledx pgkoboaurahd basker pgauraone pgafour pgnexus7 pgthinmob caslon"
 }
 
 function set_pgA4()
 {
     set_tags "afterpartnewpage papernewpage noquiz basker pgafour"
-    unset_tags "bible beforepartnewpage pghanlin pgcrownq pgkobomini pgkindledx garamond pgkoboaurahd pgauraone pgnexus7 pgnexus10 pgthinmob"
+    unset_tags "bible beforepartnewpage pghanlin pgcrownq pgkobomini pgkindledx garamond pgkoboaurahd pgauraone pgnexus7 pgnexus10 pgthinmob caslon"
 }
 
 function build_all()
@@ -103,14 +103,13 @@ function build_all()
        set_pg${fmt}
        echo -n "$fmt: "
        make clean
-       make -s
+       /usr/bin/time -f 'real: %E, user: %Us, system: %Ss' make -s
        if [ $? -eq 0 ] ; then
          mv -f ${MOD}.pdf ${outdir}/${OUTFILE}-${fmt}.pdf
        else
           echo "ERROR: Build failed, please examine the logs"
           exit
        fi
-       echo "OK"
     done
 }
 
